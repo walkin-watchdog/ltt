b0fe7c165ceae8d9d812dbd6e8ee7e0709ee1326
walkin-watchdog <info@walkinwatchdog.com>
2025-06-25 00:01:22 +0530

    Update Ignore file

--- backend/prisma/migrations/20250623111919_second/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "abandoned_carts" ADD CONSTRAINT "abandoned_carts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
